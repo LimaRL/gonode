f1292e7bcfbfded41f36208948f154ffdcc91f7c
Rafael Lima <rafael.ariquemes@gmail.com>
2019-05-07 11:08:42 -0300

    Primeiro envio

---
+++ modulo-03/src/config/mail.js
@@ -0,0 +1,9 @@
+module.exports = {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '21d51279c3eab7',
+    pass: 'ea1d1b7975bf7f'
+  }
+}
---
+++ modulo-03/src/server.js
@@ -0,0 +1,55 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const Youch = require('youch')
+const validate = require('express-validation')
+const databaseConfig = require('./config/database')
+
+class App {
+  constructor () {
+    this.express = express()
+    this.isDev = process.env.NODE_ENV !== 'production'
+
+    this.database()
+    this.middlewares()
+    this.routes()
+    this.exception()
+  }
+
+  async database () {
+    await mongoose
+      .connect(databaseConfig.uri, {
+        useCreateIndex: true,
+        useNewUrlParser: true
+      })
+      .then(() => console.log('Mongodb connected'))
+      .catch(err => console.log('Mongodb error: ', err))
+  }
+
+  middlewares () {
+    this.express.use(express.json())
+  }
+
+  routes () {
+    this.express.use(require('./routes'))
+  }
+
+  exception () {
+    this.express.use(async (err, req, res, next) => {
+      if (err instanceof validate.ValidationError) {
+        return res.status(err.status).json(err)
+      }
+
+      if (process.env.NODE_ENV !== 'production') {
+        const youch = new Youch(err)
+
+        return res.json(await youch.toJSON())
+      }
+
+      return res
+        .status(err.status || 500)
+        .json({ error: 'Internal Server Error' })
+    })
+  }
+}
+
+module.exports = new App().express
